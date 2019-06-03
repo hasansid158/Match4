b36c1e804765fb189e87eca53bf307b02819f39d
hasansid158 <48666786+hasansid158@users.noreply.github.com>
2019-06-03 15:49:51 +1000

    Match 4 Project

---
+++ Match4Project/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Match4Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Width = 503;
+            this.Height = 525;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.DoubleBuffered = true;
+            this.Paint += Form1_Paint;
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            int posX = 0, posY = 0;
+
+            for (int i = 0; i < 7; i++)
+            {
+                for (int x = 0; x < 7; x++)
+                {
+                    e.Graphics.DrawRectangle(Pens.Black, posX, posY, 65, 65);
+                    posX += 70;
+                }
+                posY += 70;
+                posX = 0;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.Refresh();
+        }
+    }
+}
---
+++ Match4Project/LinkedList.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Match4Project
+{
+    class LinkedList<T> where T : IComparable<T>
+    {
+        private Node head;
+        private int length = 0;
+
+        private class Node
+        {
+            public Node(T data)
+            {
+                this.data = data;
+            }
+            public T data;
+            public Node next, prev;
+        }
+
+        public int Length
+        {
+            get
+            {
+                Node current = head;
+                while (current != null)
+                {
+                    current = current.next;
+                    length++;
+                }
+                return length;
+            }
+        }
+
+        public void Add(T data)
+        {
+            Node newNode = new Node(data);
+            newNode.next = head;
+            head = newNode;
+        }
+
+        public T Get(int index)
+        {
+            Node current = head;
+            int currentInd = 0;
+            T value = default(T);
+
+            while (current != null)
+            {
+                if (index == currentInd)
+                {
+                    value = current.data;
+                }
+
+                currentInd++;
+                current = current.next;
+            }
+            return value;
+        }
+
+        public bool Contains(T data)
+        {
+            Node current = head;
+            bool value = false;
+
+            while (current != null)
+            {
+                if (current.data.Equals(data))
+                {
+                    value = true;
+                }
+
+                current = current.next;
+            }
+            return value;
+        }
+
+        public void Clear()
+        {
+            Node current = head;
+
+            while (current != null)
+            {
+                Node next = current.next;
+                current = null;
+                current = next;
+            }
+            head = null;
+            Console.WriteLine("CLEARED");
+        }
+
+        public void Remove(T data)
+        {
+            if (head == null)
+                return;
+
+            if (head.data.Equals(data))
+            {
+                Node next = head.next;
+                head = null;
+                head = next;
+            }
+            else
+            {
+                Node current = head;
+                Node prev = current.prev;
+
+                while (current != null)
+                {
+                    if (current.data.Equals(data))
+                    {
+                        prev.next = current.next;
+                    }
+                    else
+                    {
+                        prev = current;
+                    }
+                    current = current.next;
+                }
+            }
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (head == null)
+                return;
+
+            if (index == 0)
+            {
+                Node next = head.next;
+                head = null;
+                head = next;
+            }
+            else
+            {
+                int cur_index = 0;
+                Node current = head;
+                Node prev = current.prev;
+
+                while (current != null)
+                {
+                    if (cur_index == index)
+                    {
+                        prev.next = current.next;
+                        cur_index++;
+                    }
+                    else
+                    {
+                        prev = current;
+                        current = current.next;
+                        cur_index++;
+                    }
+                }
+            }
+        }
+
+        public void Sort()
+        {
+            Node current = head;
+            Node temp = head;
+            T prevVal;
+
+            if (current != null)
+            {
+                for (temp = head; temp.next != null; temp = temp.next)
+                {
+                    for (current = head; current.next != null; current = current.next)
+                    {
+                        if (current.next.data.CompareTo(current.data) <= 0)
+                        {
+                            prevVal = current.data;
+                            current.data = current.next.data;
+                            current.next.data = prevVal;
+                        }
+                    }
+                }
+            }
+        }
+
+        public void Display()
+        {
+            Node current = head;
+            while (current != null)
+            {
+                Console.WriteLine(current.data);
+                current = current.next;
+            }
+            Console.WriteLine("");
+        }
+    }
+}
